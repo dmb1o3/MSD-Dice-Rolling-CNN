a24c2fa9e43bfb8d13bb15295f62664a321e0efb
primewavehealth <primewavehealth@gmail.com>
2024-10-04 19:24:43 -0400

    Can run model and look at predictions now

--- coco_to_yolo.py
+++ coco_to_yolo.py
@@ -1,3 +1,4 @@
+from ultralytics import YOLO
 from ultralytics.data.converter import convert_coco
 import os
 
@@ -7,5 +8,28 @@ def coco_to_yolo():
     folder_path = os.getcwd() + "/Annotated-Data/" + folder_name + "/"
     convert_coco(folder_path, save_dir=folder_path + "YOLO-Data", use_segments=True)
 
+def run_model():
+    data_path = "./Annotated-Data/FPYS/data.yaml"
+    model = YOLO("yolov8n-seg.pt")
+    model.train(
+        data=data_path,
+        epochs=25,
+        batch=5,
+        name="dice_segmentation_experiment"
+    )
+
+    # Run validation to check metrics
+    val_results = model.val(data=data_path)
+    print(val_results)
+
+    # Predict on a test image
+    test_image = os.path.join(os.getcwd(), 'Annotated-Data/FPYS/images/val/image_23.jpg')
+    results = model(test_image)  # This returns predictions
+
+    for result in results:
+        result.show()
+        result.save("annotated_image.jpg")
+
+
 if __name__ == '__main__':
-    coco_to_yolo()
+    run_model()
