d1f520122b7c05c2c2499d9ac409107030e60af9
dmb1o3 <mrjoy14@gmail.com>
2024-10-21 11:46:03 -0400

    Removed function that is no longer needed

--- run.py
+++ run.py
@@ -43,63 +43,6 @@ def run_dice_roll_model(model, cropped_img, parent_image_name, crop_number):
     return dice_roll
 
 
-def run_dice_type_model():
-    # Load model
-    model_folder = "YOLOv8_Newest_run"
-    model = YOLO("./runs/segment/" + model_folder + "/weights/best.pt")
-
-    # Set up what images model should look at
-    """
-    train_image_folder = r'./Annotated-Data/YOLO/FPYS/images/Train'
-    val_image_folder = r'./Annotated-Data/YOLO/FPYS/images/Val'
-
-    image_paths = glob.glob(os.path.join(train_image_folder, '*.jpg')) + \
-                  glob.glob(os.path.join(val_image_folder, '*.jpg'))
-    """
-    image_folder = r'./demo/'
-
-    image_paths = glob.glob(os.path.join(image_folder, '*.jpg'))
-
-    result_dict = {'name': {}}
-
-    for image_path in image_paths:
-        print(f'\nRunning inference on {image_path}')
-        img = cv2.imread(image_path)
-        results = model(img)
-        results = results[0]
-        result_dict = results.to_df().to_dict()
-        if result_dict:
-            for i in result_dict['name']:
-                mask_points = np.array(list(zip(result_dict['segments'][i]['x'], result_dict['segments'][i]['y'])),
-                                       dtype=np.int32)
-                mask = np.zeros(img.shape[:2], dtype=np.uint8)  # Create a black mask
-                cv2.fillPoly(mask, [mask_points], 255)  # Fill the polygon with white
-
-                # Apply the mask to the cropped image
-                masked_img = cv2.bitwise_and(img, img, mask=mask)
-
-                # Crop by bounding box
-                # Get bounding box
-                box = result_dict['box']
-                x_min = int(box[i]['x1'])
-                y_min = int(box[i]['y1'])
-                x_max = int(box[i]['x2'])
-                y_max = int(box[i]['y2'])
-                # Crop image
-                cropped_img = masked_img[y_min:y_max, x_min:x_max]
-                # Set up file name and path to save crop
-                filename = f"{os.path.basename(image_path)[:-4]}_crop_{i}.jpg"  # Change .png to your desired format
-
-                # save_path = f'./runs/predictions/seg_and_box_newest_run/{dice_type}/'
-                save_path = f'./demo/segment/'
-                os.makedirs(save_path, exist_ok=True)
-                save_path = save_path + filename
-                # Save the cropped image
-                cv2.imwrite(save_path, cropped_img)  # Use mask.numpy() to get the image data
-                print(f"Saved cropped image to {save_path}")
-
-    return result_dict["name"]
-
 def run_dice_type_dice_roll_models():
     # Load dice type model
     dice_type_folder = "YOLOv8_Newest_run"
@@ -163,15 +106,13 @@ def run_dice_type_dice_roll_models():
 
 
 def run_models():
-    # Clear out folders
+    # If the exist clear out folders
     classify_folder = "./demo/classify"
     segment_folder = "./demo/segment"
-
     if os.path.exists(classify_folder):
         shutil.rmtree(classify_folder)
     if os.path.exists(segment_folder):
         shutil.rmtree(segment_folder)
-
 
     # Run models
     dice_type, dice_rolls = run_dice_type_dice_roll_models()
